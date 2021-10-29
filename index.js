ee1f2eb34a2f5b85e97d9e65e090b5ff1c085e25
Aaron Lu <aaronnoh0409@gmail.com>
2021-10-29 15:53:36 -0400

    dont forget about return if youre gonna use a value from the function again...

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function img(src, position, left, bottom) {
     image.style.left = left + 'px'
     image.style.bottom = bottom + 'px'
     document.body.append(image)
+    return image
 }
 
 img('assets/green-character.gif', 'fixed', 100, 100)
@@ -14,6 +15,16 @@ img('assets/pillar.png', 'fixed', 350, 100)
 img('assets/crate.png', 'fixed', 150, 200)
 img('assets/well.png', 'fixed', 500, 425)
 
+
+function newItem (src, position, left, bottom) {
+    let image = img (src, position, left, bottom)
+
+    image.addEventListener('click', function() {
+        image.remove()
+    })
+}
+
+newItem('assets/sword.png', 'fixed', 500, 405);
 
 // let sword = document.createElement('img')
 // sword.src = 'assets/sword.png'
@@ -35,7 +46,3 @@ img('assets/well.png', 'fixed', 500, 425)
 // staff.style.left = '600px'
 // staff.style.bottom = '100px'
 // document.body.append(staff)
-
-// sword.addEventListener('click', function(){
-//     sword.remove()
-// })
