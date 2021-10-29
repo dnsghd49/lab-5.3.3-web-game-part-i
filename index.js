03c21399edeaf31740a0aa4e8cd0a644560b9c38
Aaron Lu <aaronnoh0409@gmail.com>
2021-10-29 15:11:45 -0400

    orgnized images using function

--- index.js
+++ index.js
@@ -1,55 +1,19 @@
-function img() {
-    let greenCharacter = document.createElement('img')
-    greenCharacter.src = 'assets/green-character.gif'
-    greenCharacter.style.position = 'fixed'
-    greenCharacter.style.left = '100px'
-    greenCharacter.style.bottom = '100px'
-    document.body.append(greenCharacter)
+function img(src, position, left, bottom) {
+    let image = document.createElement('img')
+    image.src = src
+    image.style.position = position
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    document.body.append(image)
 }
 
-img()
+img('assets/green-character.gif', 'fixed', 100, 100)
+img('assets/pine-tree.png', 'fixed', 450, 200)
+img('assets/tree.png', 'fixed', 200, 300)
+img('assets/pillar.png', 'fixed', 350, 100)
+img('assets/crate.png', 'fixed', 150, 200)
+img('assets/well.png', 'fixed', 500, 425)
 
-// let greenCharacter = document.createElement('img')
-// greenCharacter.src = 'assets/green-character.gif'
-// greenCharacter.style.position = 'fixed'
-// greenCharacter.style.left = '100px'
-// greenCharacter.style.bottom = '100px'
-// document.body.append(greenCharacter)
-
-// let pineTree = document.createElement('img')
-// pineTree.src = 'assets/pine-tree.png'
-// pineTree.style.position = 'fixed'
-// pineTree.style.left = '450px'
-// pineTree.style.bottom = '200px'
-// document.body.append(pineTree)
-
-// let tree = document.createElement('img')
-// tree.src = 'assets/tree.png'
-// tree.style.position = 'fixed'
-// tree.style.left = '200px'
-// tree.style.bottom = '300px'
-// document.body.append(tree)
-
-// let pillar = document.createElement('img')
-// pillar.src = 'assets/pillar.png'
-// pillar.style.position = 'fixed'
-// pillar.style.left = '350px'
-// pillar.style.bottom = '100px'
-// document.body.append(pillar)
-
-// let crate = document.createElement('img')
-// crate.src = 'assets/crate.png'
-// crate.style.position = 'fixed'
-// crate.style.left = '150px'
-// crate.style.bottom = '200px'
-// document.body.append(crate)
-
-// let well = document.createElement('img')
-// well.src = 'assets/well.png'
-// well.style.position = 'fixed'
-// well.style.left = '500px'
-// well.style.bottom = '425px'
-// document.body.append(well)
 
 // let sword = document.createElement('img')
 // sword.src = 'assets/sword.png'
