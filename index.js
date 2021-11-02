d3b1fa37c7a972877c70d752f80c0895cd8abf7b
Aaron Lu <aaronnoh0409@gmail.com>
2021-11-01 22:19:27 -0400

    found out that all the positions were fixed so there's no need to use position parameter

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-function img(src, position, left, bottom) {
+function img(src, left, bottom) {
     let image = document.createElement('img')
     image.src = src
-    image.style.position = position
+    image.style.position = 'fixed'
     image.style.left = left + 'px'
     image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
 
-img('assets/green-character.gif', 'fixed', 100, 100)
-img('assets/pine-tree.png', 'fixed', 450, 200)
-img('assets/tree.png', 'fixed', 200, 300)
-img('assets/pillar.png', 'fixed', 350, 100)
-img('assets/crate.png', 'fixed', 150, 200)
-img('assets/well.png', 'fixed', 500, 425)
+img('assets/green-character.gif', 100, 100)
+img('assets/pine-tree.png', 450, 200)
+img('assets/tree.png', 200, 300)
+img('assets/pillar.png', 350, 100)
+img('assets/crate.png', 150, 200)
+img('assets/well.png', 500, 425)
 
 
 
-function newItem(src, position, left, bottom) {
-    let image = img(src, position, left, bottom)
+function newItem(src, left, bottom) {
+    let image = img(src, left, bottom)
 
     image.addEventListener('click', function () {
         image.remove()
     })
 }
 
-newItem('assets/sword.png', 'fixed', 500, 405);
-newItem('assets/shield.png', 'fixed', 165, 185);
-newItem('assets/staff.png', 'fixed', 600, 100);
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
